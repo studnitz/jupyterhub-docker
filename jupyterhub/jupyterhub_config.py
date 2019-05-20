23634df08c1f46603b3e1758b0f20d9e5fdf82e5
Alexander von Studnitz <alexander.von.studnitz@gmail.com>
2019-05-20 12:45:52 +0200

    Update juypterhub config

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -2,30 +2,11 @@
 c.JupyterHub.admin_access = True
 c.Spawner.default_url = '/lab'
 
-## Authenticator
-from oauthenticator.oauth2 import OAuthLoginHandler
-from oauthenticator.generic import GenericOAuthenticator
-from tornado.auth import OAuth2Mixin
+## Configure authentication (delagated to GitLab)
+from oauthenticator.gitlab import GitLabOAuthenticator
+c.JupyterHub.authenticator_class = GitLabOAuthenticator
 
-class UVSQMixin(OAuth2Mixin):
-    _OAUTH_AUTHORIZE_URL = 'https://jupyter.ens.uvsq.fr/c2o2b/login'
-    _OAUTH_ACCESS_TOKEN_URL = 'https://jupyter.ens.uvsq.fr/c2o2b/token'
-
-class UVSQLoginHandler(OAuthLoginHandler, UVSQMixin):
-    pass
-
-class UVSQAuthenticator(GenericOAuthenticator):
-    login_service = 'UVSQ'
-    login_handler = UVSQLoginHandler
-    client_id = '0'
-    client_secret = ''
-    userdata_url = 'https://jupyter.ens.uvsq.fr/c2o2b/userdata'
-    token_url = 'https://jupyter.ens.uvsq.fr/c2o2b/token'
-    oauth_callback_url = 'https://jupyter.ens.uvsq.fr/hub/oauth_callback'
-
-c.JupyterHub.authenticator_class = UVSQAuthenticator
-
-c.Authenticator.admin_users = { 'lucadefe' }
+c.Authenticator.admin_users = { 'studnitz' }
 
 
 ## Docker spawner
@@ -39,13 +20,13 @@ c.JupyterHub.hub_ip = os.environ['HUB_IP']
 
 # user data persistence
 # see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
-notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
+notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/joyvan'
 c.DockerSpawner.notebook_dir = notebook_dir
 c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
 
 # Other stuff
 c.Spawner.cpu_limit = 1
-c.Spawner.mem_limit = '10G'
+c.Spawner.mem_limit = '1G'
 
 
 ## Services
